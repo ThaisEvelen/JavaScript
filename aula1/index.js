7da82b056704eddb899788c81155ea348e09ae61
ThaisEvelen <thaisevelen@outlook.com>
2025-08-18 15:46:38 -0300

    tipos de variaveis

--- aula1/index.js
+++ aula1/index.js
@@ -1,2 +1,18 @@
-var name = 'Thais';
-console.log(name);
+var myName = 'Thais';
+console.log(myName);
+
+let meuPrimeiroNome = 'Meu primeiro nome é Thais';
+console.log(meuPrimeiroNome);
+
+const meuSobrenome = 'Pinto';
+console.log(meuSobrenome);
+
+myName = 'João';
+console.log(myName);
+
+meuPrimeiroNome = 'Meu nome é João';
+console.log(meuPrimeiroNome);
+
+
+const constComValor = 'da silva';
+console.log(constComValor);
---
+++ aula2/index.js
@@ -0,0 +1,7 @@
+const meuNome = 'Thais';
+const anoAtual = 2025;
+const estaEstudando = true;
+let qtdFilhos = null;
+let profissao = undefined;
+
+console.log(meuNome,anoAtual,estaEstudando,qtdFilhos,profissao);
