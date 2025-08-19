6d12aad708ec81abc483146ec054f43f22003922
ThaisEvelen <thaisevelen@outlook.com>
2025-08-19 20:11:54 -0300

    exercicio de funções

---
+++ exercicio-funções.js/exercicio.js
@@ -0,0 +1,32 @@
+function somar(numero1, numero2) {
+    const resultado = numero1 + numero2;
+    return resultado;
+}
+
+const somaDosNumeros = somar(2,4);
+console.log('soma',somaDosNumeros);
+
+
+function subtrair(numero3, numero4) {
+    const resultado =numero3 - numero4;
+    return resultado;
+}
+
+const subtracaoDosNumeros = subtrair(2,4);
+console.log('subtração',subtracaoDosNumeros);
+
+function multiplicar(numero5,numero6) {
+    const resultado = numero5 * numero6;
+    return resultado;
+}
+
+const multiplicaOsNumeros = multiplicar(2,4);
+console.log('multiplicação',multiplicaOsNumeros);
+
+function dividir(num1,num2) {
+    const resultado = num1 / num2;
+    return resultado;
+}
+
+const dividiOsNumeros = dividir(2,4);
+console.log('divisão',dividiOsNumeros);
